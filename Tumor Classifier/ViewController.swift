1c7406cb38a92ea099c81a37a27fb2b55efd978a
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-06-05 16:33:07 +0200

    Initial Commit

---
+++ Tumor Classifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tumor Classifier
+//
+//  Created by Jakub Stepien on 05/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
